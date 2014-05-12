7ba30a6b50a2009e7d378739d1c81599e6048a94
zed <zed@puppetlabs.vm>
2014-05-12 16:06:22 +0000

    ajout hosts

---
+++ modules/hosts/manifests/init.pp
@@ -0,0 +1,11 @@
+class hosts{
+
+          host { 'testing.puppetlabs.vm':
+
+              ensure       => 'present',
+              host_aliases => ['testing'],
+              ip           => '127.0.0.1',
+              target       => '/etc/hosts',
+              comment      => 'entry in /etc/hosts for testing',
+            }
+}
